790a30e3a7d637559ca68b05a6c744a5a808c095
Kostya Niekrasov <kostyanekrasov1501@gmail.com>
2024-06-11 11:02:44 +0300

    added favicon

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,16 +1,16 @@
-'use strict';
+"use strict";
 
-window.addEventListener('hashchange', () => {
-  if (window.location.hash === '#menu') {
-    document.body.classList.add('page__body--with-menu');
+window.addEventListener("hashchange", () => {
+  if (window.location.hash === "#menu") {
+    document.body.classList.add("page__body--with-menu");
   } else {
-    document.body.classList.remove('page__body--with-menu');
+    document.body.classList.remove("page__body--with-menu");
   }
 });
 
-const form = document.getElementById('contacts-form');
+const form = document.getElementById("contacts-form");
 
-form.addEventListener('submit', function handleSubmin(event) {
+form.addEventListener("submit", function handleSubmin(event) {
   event.preventDefault();
 
   form.reset();
--- src/scripts/slider.js
+++ src/scripts/slider.js
@@ -1,24 +1,22 @@
-'use strict';
+"use strict";
 
-const btnPrev = document.querySelector('.btn--prev');
-const btnNext = document.querySelector('.btn--next');
-const slides = document.querySelectorAll('.slide');
+const btnPrev = document.querySelector(".btn--prev");
+const btnNext = document.querySelector(".btn--next");
+const slides = document.querySelectorAll(".slide");
 let counter = 0;
 
-slides.forEach(
-  (slide, index) => {
-    slide.style.left = `${index * 100}%`;
-  }
-);
+slides.forEach((slide, index) => {
+  slide.style.left = `${index * 100}%`;
+});
 
-btnPrev.addEventListener('click', function() {
+btnPrev.addEventListener("click", function () {
   if (counter !== 0) {
     counter--;
     slideImage();
   }
 });
 
-btnNext.addEventListener('click', function() {
+btnNext.addEventListener("click", function () {
   if (counter < slides.length - 1) {
     counter++;
     slideImage();
@@ -26,9 +24,7 @@ btnNext.addEventListener('click', function() {
 });
 
 const slideImage = () => {
-  slides.forEach(
-    (slide) => {
-      slide.style.transform = `translateX(-${counter * 100}%)`;
-    }
-  );
+  slides.forEach((slide) => {
+    slide.style.transform = `translateX(-${counter * 100}%)`;
+  });
 };
